de7d096f1616dcd4361c92bdeaa5d4e39acb3cb5
Parabeac-Nest <admin@parabeac.com>
2022-12-15 14:15:41 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/profile_theme.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:profile/theme/profile_text_styles.g.dart';
+import 'package:profile/theme/profile_colors.g.dart';
+
+class ProfileTheme {
+
+static const TextTheme textTheme = TextTheme(headline1: ProfileTextStyles.headline1,headline2: ProfileTextStyles.headline2,headline3: ProfileTextStyles.headline3,headline4: ProfileTextStyles.headline4,headline5: ProfileTextStyles.headline5,headline6: ProfileTextStyles.headline6,subtitle1: ProfileTextStyles.subtitle1,subtitle2: ProfileTextStyles.subtitle2,bodyText1: ProfileTextStyles.bodyText1,bodyText2: ProfileTextStyles.bodyText2,button: ProfileTextStyles.button,caption: ProfileTextStyles.caption,overline: ProfileTextStyles.overline,);
+
+static const ColorScheme light = ColorScheme.light(onPrimary: ProfileColors.lightOnPrimary,primary: ProfileColors.lightPrimary,primaryContainer: ProfileColors.lightPrimaryContainer,secondary: ProfileColors.lightSecondary,onSecondary: ProfileColors.lightOnSecondary,onPrimaryContainer: ProfileColors.lightOnPrimaryContainer,secondaryContainer: ProfileColors.lightSecondaryContainer,onSecondaryContainer: ProfileColors.lightOnSecondaryContainer,onTertiary: ProfileColors.lightOnTertiary,tertiary: ProfileColors.lightTertiary,tertiaryContainer: ProfileColors.lightTertiaryContainer,onTertiaryContainer: ProfileColors.lightOnTertiaryContainer,error: ProfileColors.lightError,onError: ProfileColors.lightOnError,errorContainer: ProfileColors.lightErrorContainer,onErrorContainer: ProfileColors.lightOnErrorContainer,background: ProfileColors.lightBackground,onBackground: ProfileColors.lightOnBackground,surface: ProfileColors.lightSurface,onSurface: ProfileColors.lightOnSurface,surfaceVariant: ProfileColors.lightSurfaceVariant,onSurfaceVariant: ProfileColors.lightOnSurfaceVariant,outline: ProfileColors.lightOutline,inverseSurface: ProfileColors.lightInverseSurface,onInverseSurface: ProfileColors.lightOnInverseSurface,inversePrimary: ProfileColors.lightInversePrimary,surfaceTint: ProfileColors.lightSurfaceTint);
+
+static  ThemeData themeDataLight = ThemeData(textTheme: textTheme, colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: ProfileColors.darkPrimary,onPrimary: ProfileColors.darkOnPrimary,primaryContainer: ProfileColors.darkPrimaryContainer,onPrimaryContainer: ProfileColors.darkOnPrimaryContainer,secondary: ProfileColors.darkSecondary,onSecondary: ProfileColors.darkOnSecondary,secondaryContainer: ProfileColors.darkSecondaryContainer,onSecondaryContainer: ProfileColors.darkOnSecondaryContainer,tertiary: ProfileColors.darkTertiary,onTertiary: ProfileColors.darkOnTertiary,tertiaryContainer: ProfileColors.darkTertiaryContainer,onTertiaryContainer: ProfileColors.darkOnTertiaryContainer,error: ProfileColors.darkError,onError: ProfileColors.darkOnError,errorContainer: ProfileColors.darkErrorContainer,onErrorContainer: ProfileColors.darkOnErrorContainer,background: ProfileColors.darkBackground,onBackground: ProfileColors.darkOnBackground,surface: ProfileColors.darkSurface,onSurface: ProfileColors.darkOnSurface,surfaceVariant: ProfileColors.darkSurfaceVariant,outline: ProfileColors.darkOutline,inverseSurface: ProfileColors.darkInverseSurface,inversePrimary: ProfileColors.darkInversePrimary,surfaceTint: ProfileColors.darkSurfaceTint,onSurfaceVariant: ProfileColors.darkOnSurfaceVariant,onInverseSurface: ProfileColors.darkOnInverseSurface);
+
+static  ThemeData themeDataDark = ThemeData(textTheme: textTheme, colorScheme: dark,);
+}
